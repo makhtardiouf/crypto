92d041041c74d256bf8ebaca3d88513bcd91e0aa
Makhtar Diouf <makhtar.diouf@gmail.com>
2021-11-01 12:11:28 +0000

    Git init

---
+++ js/hardhat/contracts/TokenElh.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: GPL-3.0
+/*
+ * ELH Smart Contract
+ * Ref https://hardhat.org/tutorial/writing-and-compiling-contracts.html
+*/
+pragma solidity ^0.8;
+import "hardhat/console.sol";
+// Main building block for Smart Contract
+
+contract TokenElh {
+
+    string public name = "Elhadji's Token";
+    string public symbol = "ELH";
+
+    uint public totalSupply = 1000;
+
+    // An address type variable is used to store ethereum accounts
+    address public owner;
+
+    // Map balances to accounts
+    mapping(address => uint) balances;
+
+    /**
+     * Contract initialization.
+     *
+     * The `constructor` is executed only once when the contract is created.
+     */
+     constructor() {
+        // The totalSupply is assigned to transaction sender,
+        // which is the account that is deploying the contract
+        balances[msg.sender] = totalSupply;
+        owner = msg.sender;
+        //console.log("Owner address:", owner);
+     }
+
+    /**
+     * A function to transfer tokens.
+     *
+     * The `external` modifier makes a function *only* callable from outside
+     * the contract.
+     */
+    function transfer(address to, uint amount) external {
+
+        require(balances[msg.sender] >= amount, "Balance is insufficient");
+
+        // Transfer
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+        
+        // Could simply use owner.transfer() ?
+    }
+
+    /**
+     * Read only function to retrieve the token balance of a given account.
+     *
+     * The `view` modifier indicates that it doesn't modify the contract's
+     * state, which allows us to call it without executing a transaction.
+     */
+    function balanceOf(address account) external view returns (uint) {
+        return balances[account];
+    }
+
+}
---
+++ js/hardhat/test/TokenElh.js
@@ -0,0 +1,62 @@
+/*
+ * Workaround of nodejs 17 bug
+ * export NODE_OPTIONS=--openssl-legacy-provider
+ * npx hardhat test
+ * 
+ * Signer in ethers.js is an object that represents an Ethereum account
+*/
+const { ethers } = require("hardhat");  // By default available in the global scope
+const { expect } = require("chai");
+
+
+describe("ELH Token contract", function () {
+    let tok;
+    let elhContract;
+    let owner;
+    let addr1;
+    let addr2;
+    let addrs;
+
+    // Run before each test, re-deploying the contract every time.
+    this.beforeEach(async function() {
+        [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+
+        tok = await ethers.getContractFactory("TokenElh");
+        elhContract = await tok.deploy();
+    });
+
+    // Nested subsections 
+    describe("Deployment", function () {
+        it("Set the right owner", async function(){
+            expect(await elhContract.owner()).to.equal(owner.address);
+        });
+
+        it("Assign the total supply of tokens to the owner", async function () {
+
+            const ownerBalance = await elhContract.balanceOf(owner.address);
+            console.log(`\tBalance of ${owner.address} ${ownerBalance} ETH`)
+
+            expect(await elhContract.totalSupply()).to.equal(ownerBalance);
+        });
+    });
+
+    // Transfer from a different source Acc, using connect()
+
+    describe("Transactions", function () {
+        it("Transfer tokens between accounts", async function () {
+            console.log("From", addr1.address, "to", addr2.address);
+
+            let amount = 10;
+            // Transfer 50 tokens from owner to addr1
+            await elhContract.transfer(addr1.address, amount);
+            let bal = await elhContract.balanceOf(addr1.address);
+            expect(bal).to.equal(amount);
+            console.log("Balance", bal, "ETH");
+
+            // Transfer 50 tokens from addr1 to addr2
+            await elhContract.connect(addr1).transfer(addr2.address, amount);
+            expect(await elhContract.balanceOf(addr2.address)).to.equal(amount);
+        });
+    });
+
+});
